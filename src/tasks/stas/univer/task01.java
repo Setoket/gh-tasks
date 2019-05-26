54fc54cf0f9ea7000e4b166c194bf74da2fdcec0
Setoket <50265460+Setoket@users.noreply.github.com>
2019-05-26 17:06:29 +0300

    comment01
    comment02

--- src/tasks/stas/univer/task01.java
+++ src/tasks/stas/univer/task01.java
@@ -1,4 +1,18 @@
 package tasks.stas.univer;
 
 public class task01 {
+    public static void main(String[] args) {
+        Mydate mydate = new Mydate();
+        System.out.print(mydate.month + " ");
+        System.out.print(mydate.day + " ");
+        System.out.print(mydate.year);
+    }
+
+    public static class Mydate {
+
+        String month = "January";
+        int day = 10;
+        int year = 1997;
+    }
+
 }
