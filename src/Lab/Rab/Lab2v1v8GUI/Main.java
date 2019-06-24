d8647953e417d054c769583087a801acd452a5db
GHLeo <ggleonid@gmail.com>
2019-06-24 13:41:42 +0500

    Laba 2v2v8 ready!

--- src/Lab/Rab/Lab2v1v8GUI/Main.java
+++ src/Lab/Rab/Lab2v1v8GUI/Main.java
@@ -2,7 +2,8 @@ package Lab.Rab.Lab2v1v8GUI;
 
 public class Main {
     public  static void main(String[] args) {
-SimpleGUI app = new SimpleGUI();
+//SimpleGUI app = new SimpleGUI();
+GUI app = new GUI();
 app.setVisible(true);
     }
 }
